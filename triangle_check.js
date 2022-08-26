ffbcb7574bc4f670015cf813e2f15305ed90d8c8
Jagrut Sharma <jagrut.sharma1@gmail.com>
2022-08-26 20:52:44 +0530

    style: nav fixed

--- triangle_check.js
+++ triangle_check.js
@@ -11,19 +11,17 @@ function isTriangle(){
     if (Number(firstAngle.value) <= 0 && Number(secondAngle.value) <= 0 && Number(thirdAngle.value) <= 0) {
         displayMessage("Enter values above zero");
         result.style.color = "red";
-        result.style.fontWeight = "bold";
-        return 0;
     }
     else {
         const sum = Number(firstAngle.value) + Number(secondAngle.value) + Number(thirdAngle.value);
-        result.style.color = "unset";
-        result.style.fontWeight = "unset";
         if (sum === 180){
-            displayMessage("It is indeed a triangle👍");
+            displayMessage("It is indeed a triangle 👍");
+            result.style.color = "green";
         }
 
         else{
             displayMessage("Not a triangle");
+            result.style.color = "blue";
         }
     }
 }
