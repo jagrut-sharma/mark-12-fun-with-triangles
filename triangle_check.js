0fb72b92ba9a6ef97cf19e7d9119b94ad4f4df8d
Jagrut Sharma <jagrut.sharma1@gmail.com>
2022-08-27 12:23:57 +0530

    bug: fixed logic of  triangle check js

--- triangle_check.js
+++ triangle_check.js
@@ -7,27 +7,49 @@ const result = document.querySelector(".result");
 checkBtn.addEventListener("click", isTriangle);
 
 function isTriangle(){
-    
-    if (Number(firstAngle.value) < 1 && Number(secondAngle.value) < 1 && Number(thirdAngle.value) < 1) {
-        displayMessage("Enter values above zero");
-        result.style.color = "red";
-    }
-    else {
-        console.log(secondAngle.value);
-        console.log(thirdAngle.value);
-        const sum = Number(firstAngle.value) + Number(secondAngle.value) + Number(thirdAngle.value);
-        if (sum === 180){
+    angleOne = Number(firstAngle.value);
+    angleTwo = Number(secondAngle.value);
+    angleThree = Number(thirdAngle.value);
+
+    if(angleOne > 0 && angleTwo > 0 && angleThree > 0){
+        const sum = angleOne + angleTwo + angleThree;
+
+        if(sum === 180) {
             displayMessage("It is indeed a triangle 👍");
             result.style.color = "green";
         }
-
         else{
-            displayMessage("Not a triangle");
+            displayMessage("It is not a triangle");
             result.style.color = "blue";
         }
     }
+    else{
+        displayMessage("Enter values above zero");
+        result.style.color = "red";
+    }
+    
 }
 
 function displayMessage(message){
     result.innerText = message;
 }
+
+// result.style.color = "red";
+// if (Number(firstAngle.value) < 1 && Number(secondAngle.value) < 1 && Number(thirdAngle.value) < 1) {
+//     displayMessage("Enter values above zero");
+//     result.style.color = "red";
+// }
+// else {
+//     console.log(secondAngle.value);
+//     console.log(thirdAngle.value);
+//     const sum = Number(firstAngle.value) + Number(secondAngle.value) + Number(thirdAngle.value);
+//     if (sum === 180){
+//         displayMessage("It is indeed a triangle 👍");
+//         result.style.color = "green";
+//     }
+
+//     else{
+//         displayMessage("Not a triangle");
+//         result.style.color = "blue";
+//     }
+// }
