446b0f141f23b5d563144c4fcce573b488a9460c
Jagrut Sharma <jagrut.sharma1@gmail.com>
2022-08-27 12:08:40 +0530

    style: completed css parts

--- triangle_check.js
+++ triangle_check.js
@@ -7,12 +7,14 @@ const result = document.querySelector(".result");
 checkBtn.addEventListener("click", isTriangle);
 
 function isTriangle(){
-
-    if (Number(firstAngle.value) <= 0 && Number(secondAngle.value) <= 0 && Number(thirdAngle.value) <= 0) {
+    
+    if (Number(firstAngle.value) < 1 && Number(secondAngle.value) < 1 && Number(thirdAngle.value) < 1) {
         displayMessage("Enter values above zero");
         result.style.color = "red";
     }
     else {
+        console.log(secondAngle.value);
+        console.log(thirdAngle.value);
         const sum = Number(firstAngle.value) + Number(secondAngle.value) + Number(thirdAngle.value);
         if (sum === 180){
             displayMessage("It is indeed a triangle 👍");
